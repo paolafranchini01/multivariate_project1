b2b2580f33443269e08ab2413474eba7af04eced
nicoleangel3 <nicole.angel@studenti.unimi.it>
2025-04-22 23:51:31 +0200

    Adding a plot

--- multivariate_project1/scripts/01-EDA.R
+++ multivariate_project1/scripts/01-EDA.R
@@ -14,15 +14,17 @@ summary(ESS11$ccnthum)
 
 #There are some missing/don't know, so I'm going to clean the data
 
-ESS11$ccnthum[ESS11$ccnthum %in% c(77, 88, 99)] <- NA
-
+ESS11$ccnthum[ESS11$ccnthum %in% c(55, 77, 88, 99)] <- NA
+#I've added the "climate change is not happening" responses as NAs because they were
+#very few and also didn't seem to work with the following recoding.
+#We should look into it though! I think it may be methodologically inappropriate.
 
 #I want to get a frequency table, so I'm going to make my variable a factor
 #based on the answers of the questionnaire
 
 clim_change_cause <- factor(ESS11$ccnthum,
-                        levels = c(1, 2, 3, 4, 5, 6),
-                        labels = c("Entirely natural", "Mainly natural", "Equally natural and antropic", "Mainly human", "Entirely human", "No change happening"))
+                        levels = c(1, 2, 3, 4, 5),
+                        labels = c("Entirely natural", "Mainly natural", "Equally natural and human", "Mainly human", "Entirely human"))
 
 clim_change_cause
 
@@ -33,3 +35,15 @@ summary(clim_change_cause)
 prop.table(table(ESS11$ccnthum))
 
 prop.table(table(clim_change_cause))
+
+
+#visualizing the distribution
+ggplot(ESS11, aes(x = clim_change_cause)) +
+  geom_bar(color = "purple", fill = "purple") +
+  theme(axis.text.x = element_text(size = 8, angle = 45, hjust = 1),
+         axis.text.y = element_text(size = 12)) +
+  labs(title = "Beliefs about causes of climate change",
+       x = "Natural vs. human",
+       y = "Count")
+
+ggsave("plots/climate_causes_beliefs.png", width = 8, height = 6, dpi = 300)
